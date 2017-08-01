ee51b0967c5153b7535c175e671c9a1579d1a62f
nvwa-xt <nvwa-xt@qq.com>
2017-08-01 15:49:47 +0800

    `data`

--- myapp.py
+++ myapp.py
@@ -1,9 +1,25 @@
-from flask import Flask, g, request
+from flask import Flask,request, make_response  
+import hashlib 
+import time
 
 app = Flask(__name__)
 app.debug = True
 
-@app.route('/')
+@app.route('/xiaohui')
 def hello():
     return "Hello, world! - Flask- xiaohui"
 
+@app.route('/', methods = ['GET', 'POST'] )  
+def wechat_auth():  
+  if request.method == 'GET':  
+    token = 'aomeng12348765' # your token  
+    query = request.args  # GET 方法附上的参数  
+    signature = query.get('signature', '')  
+    timestamp = query.get('timestamp', '')  
+    nonce = query.get('nonce', '')  
+    echostr = query.get('echostr', '')  
+    s = [timestamp, nonce, token]  
+    s.sort()  
+    s = ''.join(s)  
+    if ( hashlib.sha1(s).hexdigest() == signature ):    
+      return make_response(echostr)  
